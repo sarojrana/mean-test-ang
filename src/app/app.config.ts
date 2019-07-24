9bd157265f58f1369bf9672956c4dfb9768907c7
Saroj Rana <rana.saroj95@gmail.com>
2019-07-24 19:07:11 +0545

    Auto-generated commit

--- src/app/app.config.ts
+++
@@ -1,7 +0,0 @@
-import { environment } from 'src/environments/environment';
-
-
-export let config = {
-  serverUrl: environment.serverUrl,
-  serverApiUrl: environment.serverApiUrl,
-};
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  serverUrl: 'https://sarojrana-mean.herokuapp.com',
-  serverApiUrl: 'https://sarojrana-mean.herokuapp.com/'
-};
